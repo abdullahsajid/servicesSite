25a7c1d41945307dd01003bd5f9b863c092033d6
Abdullah sajid <abdullahsajid521@gmail.com>
2023-11-05 18:58:47 +0500

    popup model change

--- js/script.js
+++ js/script.js
@@ -7,6 +7,9 @@ openModel.addEventListener('click',()=>{
     openModel.style.display = "none"
     closeModel.style.display = "flex"
     document.querySelector('#rm-mr').classList.add('mr-remove')
+    document.body.classList.add('body-model-open')
+    document.querySelector('.model-open').classList.add('model-open-mt')
+    document.querySelector('.option-comp').classList.add('resp-model-open-opacity')
 })
 
 closeModel.addEventListener('click',()=>{
@@ -14,4 +17,7 @@ closeModel.addEventListener('click',()=>{
     openModel.style.display = "flex"
     closeModel.style.display = "none"
     document.querySelector('#rm-mr').classList.remove('mr-remove')
+    document.body.classList.remove('body-model-open')
+    document.querySelector('.model-open').classList.remove('model-open-mt')
+    document.querySelector('.option-comp').classList.remove('resp-model-open-opacity')
 })
